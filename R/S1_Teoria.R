1e5cae76abd200fdb6381319f3eb106ec21d7e78
Alejandro Diaz <alejandro.galache@gmail.com>
2017-10-25 21:13:57 +0200

    Session 2 of R course

--- R/S1_Teoria.R
+++ R/S1_Teoria.R
@@ -19,6 +19,7 @@ mode(a)
 ls()
 objects()
 rm(list=ls())
+rm(a)
 
 ###################
 ## 4. Data Types ##
@@ -41,7 +42,8 @@ f <- c(a,d)
 class(f)
 
 #### Arithmetic operations
-a+b; a+c;
+a+b; 
+a+c
 a*5
 sum(a)
 mean(a)
@@ -49,6 +51,11 @@ median(a)
 a+median(a)
 
 #### Selection
+
+a
+bool <- c(F,T,T,T)
+a[bool]
+
 a[2]
 a[2:3]
 a[-(2:3)]
@@ -57,11 +64,17 @@ a[c(2,4)]
 a[a > 3]
 a[a %in% c(1:3)]
 
-x <- c(1,2,3,4)
+x <- c(1,1,1,1,11,2,3,4)
 names(x) <- c("a","b","c","d")
 x["a"]
-
 rm(list=ls())
+
+
+sort(x)
+sort(x,decreasing = T)
+table(x)
+unique(x)
+
 
 ## 4.2 Matrix
 
@@ -71,6 +84,7 @@ B <- matrix(nrow = 2, ncol = 3, rep(1:2, length(6)))
 C <- matrix(nrow = 2, ncol = 3, rep(1:2, length(6)), byrow = T)
 
 #### Selection
+A
 A[1,]
 B[1,2]
 C[1,1:2]
@@ -83,14 +97,19 @@ crossprod(B,C)
 ## 4.3 List
 v = list(a=c(2, 3, 5), b=c("a", "e", "i", "o", "u"))
 
+v$c <- 23
+
+v
 #### Selection
 v$a
 v[[1]]
 v$a[1]
 
 attach(v)
-a
+a[1]
+b[2]
 detach(v)
+search()
 
 ## 4.4 Data Frames
 help("mtcars")
@@ -98,16 +117,22 @@ str(mtcars)
 summary(mtcars)
 
 DF_mtcars <- mtcars
+mtcars$l <- 0
 
 #### Selection
-DF_mtcars[["mpg"]]
+DF_mtcars["mpg"]
+DF_mtcars$mpg
+
 DF_mtcars[1,]
 DF_mtcars[1:6,]
 head(DF_mtcars)
 DF_mtcars[3,c("hp","cyl")]
 
 #### Add Column
+head(DF_mtcars)
 DF_mtcars <- cbind(new = 0, DF_mtcars)
+str(DF_mtcars)
+DF_mtcars$`0` <- NULL
 DF_mtcars$new_2 <- "A"
 
 #### Delete Column
@@ -115,7 +140,8 @@ DF_mtcars$new <- NULL
 DF_mtcars$new_2 <- NULL
 
 #### Subset
-DF_mtcars[DF_mtcars$hp < 175 & DF_mtcars$cyl >= 6,]
+DF_mtcars_2 <- DF_mtcars[DF_mtcars$hp < 175 & DF_mtcars$cyl >= 6,]
+DF_mtcars_2
 
 #### Order
 DF_mtcars[order(DF_mtcars$hp),]
@@ -130,11 +156,14 @@ DF_mtcars$metric_3 <- apply(DF_mtcars[,c("gear","carb")], MARGIN = 1, function(x
 # install.packages("data.table")
 library("data.table")
 DT = data.table(ID = c("b","b","b","a","a","c"), a = 1:6, b = 7:12, c = 13:18)
-DT_mtcars <- as.data.table(mtcars)
+DT_mtcars_1 <- as.data.table(mtcars, keep.rownames = "rn")
 str(DT_mtcars)
 
+rowMeans(DT_mtcars)
+head(DT_mtcars)
+
 #### Subset rows in "i"
-DT_mtcars[hp < 175 & cyl >= 6]
+DT_mtcars[hp < 175 & cyl >= 6,]
 
 #### Order
 DT_mtcars[order(hp,-cyl)]
@@ -148,7 +177,36 @@ DT_mtcars[hp < 175 & cyl >= 6,sum(wt)]
 setkey(DT_mtcars,cyl)
 key(DT_mtcars)
 
-####  Subset with key
+#### Subset with key
 DT_mtcars[.(8)]
 DT_mtcars[.(6), mult = "first"]
 DT_mtcars[.(6), mult = "last"]
+
+## 5.1 Funciones
+#### Ejemplo función suma de dos números
+f1 <- function(x,y) {
+  return(x+y)
+}
+
+f1(1,2)
+f1(7,2)
+
+#### Mismo ejemplo
+f2 <- function(x,y=1) {
+  x+y
+}
+
+f2(1)
+f2(7,2)
+
+## 5.2 Secuencia de control
+#### Ejemplo for
+for(i in 1:10) {
+  print(paste("Iteracción For:",i,""))
+}
+#### Ejemplo While
+x = 1
+while(x <= 10) {
+  print(paste("Iteracción While:",x,""))
+  x = x+1
+}
---
+++ R/S2_Teoria.R
@@ -0,0 +1,95 @@
+#########################
+##  Syllabus Session 2 ##
+#########################
+
+"
+- Carga de datos
+- Google Analytics Reporting API
+- Gráficos
+- 
+"
+
+####################
+## Carga de datos ##
+####################
+library(data.table)
+
+help(iris)
+str(iris)
+dim(iris)
+
+write.table(iris, file = "iris.test.csv", sep = ";", row.names=FALSE)
+DT<-fread("iris.test.csv",sep = ";", fill = T)
+DT<-fread("iris.test.csv",sep = ";", fill = T, select = c("Species","Sepal.Width"))
+head(DT)
+
+# install.packages("rjson")
+library(rjson)
+## Read JSON file
+json_example <- fromJSON('{"id":1,"name":"leche","price":12.5,"tags":["desnatada","sin lactosa"]}')
+class(json_example)
+cat(paste("El producto con Id:",json_example$id, sep = " ","tiene un precio de",json_example$price))
+json_example$tags[1]
+json_example$tags[2]
+
+####################
+###### GA API ######
+####################
+
+# install.packages("RCurl")
+# install.packages("jsonlite")
+# install.packages("bitops")
+# install.packages("devtools")
+# library(devtools) # Cargamos devtools
+# install_github("rga", "skardhamar")
+
+library(rga)
+
+client.id <- "852713182122-1k6rcppuuit6v1cluqo7odplbakb5i5c.apps.googleusercontent.com"
+client.secret <- "DNvhZYMSKKgxNAwnvJzMz4Xc"
+
+# Instanciamos
+rga.open(instance="ga", client.id=client.id, client.secret=client.secret)
+
+# Query A
+date.start <- "2017-01-01"
+date.end <- "2017-05-31"
+
+datos.a <- ga$getData(84705203, date.start, date.end, 
+                      metrics = "ga:hits, ga:sessions, ga:users, ga:pageviews, ga:avgSessionDuration", 
+                      dimensions = "ga:date, ga:deviceCategory", 
+                      sort = "", filters = "ga:medium==organic", segment = "",
+                      start = 1)
+
+nrow(datos.a)
+head(datos.a)
+summary(datos.a)
+
+# write.csv(datos, file = "ReportGA.csv")
+
+####################
+###### GRAPHS ######
+####################
+
+f = c("H","M","M","M","M","H","M","H","M","M")
+sex = factor(f,levels=c("H","M"),ordered=TRUE)
+table(f)
+plot(sex, main = "Factor Plot")
+
+c <- sample(1:1000, size = 200, replace = FALSE)
+plot(c, main = "Numeric Vector", xlab = "Vector Pos", ylab = "Vector Value")
+
+m <- matrix(nrow = 6, ncol = 5, sample(1:3, replace = FALSE))
+plot(m, main = "Matrix")
+
+#### Hist
+d <- sample(c(1,2,3), size = 100, replace = T)
+hist(d, main = "Histogram of Vector d", ylab = "Total", xlab = "Value", xlim = c(1,3), breaks = c(1,2,3))
+
+BMI<-rnorm(n=1000, mean = 25.6, sd=2.2)
+hist(BMI, breaks = 25)
+
+#### Heatmap
+x <- 1:10; y <- 1:15; z <- matrix(nrow = 10, ncol = 15, sample(1:30, replace = T))
+image(x,y,z)
+
